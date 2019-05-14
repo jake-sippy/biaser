f2cf9fe7a492438588a06685e8509b2fdf25f1fe
Jake Sippy <jsippy@cs.washington.edu>
2019-05-13 21:49:23 -0700

    created file for model comparison

--- bias.py
+++ bias.py
@@ -1,11 +1,23 @@
-from __future__ import print_function
-
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+
+def load_dataset(csv_path):
+    with open(csv_path, 'r') as f:
+        sentences = []
+        labels = []
+        for line in f:
+            split = line.split(',')
+            sentences.append(split[1])
+            labels.append(split[0])
+    return sentences, labels
 
 
 def bias_dataset(X, y, rule, y_new):
-    """Introduce bias into a dataset according to some rule "r"
+    """Introduce bias into a dataset according to some rule
 
     Args:
         X -- a numpy array of n examples by d features
@@ -24,4 +36,17 @@ def bias_dataset(X, y, rule, y_new):
     return mask, y_bias
 
 
+if __name__ == '__main__':
+    sentences, labels = load_dataset('data/reviews_Video_Games_5.csv')
+    x_train, x_test, y_train, y_test = train_test_split(sentences,
+                                                        labels,
+                                                        train_size=0.8,
+                                                        test_size=0.2)
+    model = Pipeline(steps=[
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('tree', MLPClassifier()),
+    ])
 
+    model.fit(x_train, y_train)
+    print(model.score(x_test, y_test))
