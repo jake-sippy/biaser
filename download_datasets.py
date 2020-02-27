815264d5ec604f2d1e22b4e51114172762530155
jake@sippy.dev <jake@sippy.dev>
2020-02-27 15:23:37 -0800

    Updated to include new datsets

--- download_datasets.py
+++ download_datasets.py
@@ -7,6 +7,7 @@ import pandas as pd
 from urllib.request import urlopen
 from sklearn.datasets import fetch_20newsgroups
 
+DATASET_DIR = 'datasets'
 
 # Load the IMDb Reviews dataset
 def load_imdb(path):
@@ -31,7 +32,7 @@ def load_imdb(path):
 
 
 # Load the Amazon Cell Phones and Accessories Reviews dataset
-def load_amazon(path):
+def load_amazon_cell(path):
     print('Loading the Amazon Cell Phones and Accessories Reviews dataset...')
     url = 'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/' \
           'reviews_Cell_Phones_and_Accessories_5.json.gz'
@@ -43,7 +44,7 @@ def load_amazon(path):
     dataset['overall'] = dataset['overall'].map(
             {1:0, 2:0, 4:1, 5:1}
     )
-    filename = os.path.join(path, 'amazon.csv')
+    filename = os.path.join(path, 'amazon_cell.csv')
     dataset.to_csv(filename, index=False, header=False,
             columns=['reviewText', 'overall'])
 
@@ -65,37 +66,48 @@ def load_amazon_home(path):
     dataset.to_csv(filename, index=False, header=False,
             columns=['reviewText', 'overall'])
 
-# Load the 20 Newsgroups dataset
-def load_newsgroups(path):
+# Load the 20 Newsgroups dataset Atheism v. Christianity
+def load_newsgroups_atheism(path):
     data = fetch_20newsgroups(
             remove=('headers', 'footers', 'quotes'),
             categories=['alt.atheism', 'soc.religion.christian']
     )
-    df = pd.DataFrame(data=[i for i in zip(data.data, data.target)])
-    filename = os.path.join(path, 'newsgroups.csv')
+    df = pd.DataFrame(data=zip(data.data, data.target))
+    filename = os.path.join(path, 'newsgroups_atheism.csv')
+    df.to_csv(filename, index=False, header=False)
+
+
+# Load the 20 Newsgroups dataset Baseball v. Hockey
+def load_newsgroups_baseball(path):
+    data = fetch_20newsgroups(
+            remove=('headers', 'footers', 'quotes'),
+            categories=['rec.sport.baseball', 'rec.sport.hockey']
+    )
+    df = pd.DataFrame(data=zip(data.data, data.target))
+    filename = os.path.join(path, 'newsgroups_baseball.csv')
+    df.to_csv(filename, index=False, header=False)
+
+
+# Load the 20 Newsgroups dataset IBM v. Mac
+def load_newsgroups_ibm(path):
+    data = fetch_20newsgroups(
+            remove=('headers', 'footers', 'quotes'),
+            categories=['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware']
+    )
+    df = pd.DataFrame(data=zip(data.data, data.target))
+    filename = os.path.join(path, 'newsgroups_ibm.csv')
     df.to_csv(filename, index=False, header=False)
 
 
 if __name__ == '__main__':
-    description = 'Load all datasets and processes them to be in a consistent' \
-                  ' format and stores them in the specified directory.'
-    parser = argparse.ArgumentParser(description=description)
-    default_dir = 'datasets'
-    parser.add_argument(
-            '--dir',
-            type=str,
-            metavar='DIR',
-            default=default_dir,
-            help='Path to save datasets (default = {}/)'.format(default_dir)
-    )
-    args = parser.parse_args()
-
-    directory = args.dir
+    directory = DATASET_DIR
     if not os.path.exists(directory):
         os.mkdir(directory)
 
     # Load datasets
     # load_imdb(directory)
-    # load_amazon(directory)
-    load_amazon_home(directory)
-    # load_newsgroups(directory)
+    # load_amazon_cell(directory)
+    # load_amazon_home(directory)
+    # load_newsgroups_atheism(directory)
+    load_newsgroups_baseball(directory)
+    load_newsgroups_ibm(directory)
