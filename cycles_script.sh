d72beb9281e214bff395ca87efa840ee47a3c573
jake@sippy.dev <jake@sippy.dev>
2020-02-28 14:49:32 -0800

    Finished script for deployment

--- cycles_script.sh
+++ cycles_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+git clone https://github.com/jake-sippy/biaser.git
+cd biaser
+
+source $(conda info --base)/etc/profile.d/conda.sh
 conda env create -f environment.yaml
 conda activate bias
-python download_datasets.py
-for dataset in datasets/*; do
-    echo "python full_test.py $dataset 0 10"
-    python full_test.py $dataset 0 10
-done
+
+# python download_datasets.py
+echo "python full_test.py 0 10"
+python full_test.py $dataset 0 10 $1
